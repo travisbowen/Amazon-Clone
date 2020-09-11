f32492d6afab0dd1ffc89536cd06431f5a699eaf
Travis Bowen <Travisbowen23@gmail.com>
2020-09-11 14:32:57 -0700

    Added initial home component and completed background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			{/* Header */}
 			<Header />
 			{/* Home */}
+			<Home />
 		</div>
 	);
 }
