42c640d8ca5268e6fe06b0c3f425ec593017831b
Travis Bowen <Travisbowen23@gmail.com>
2020-09-11 11:52:32 -0700

    Finished header bar and styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
-import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
 	return (
@@ -19,12 +20,19 @@ function Header() {
 			<div className='header__nav'>
 				<div className='header__options'>
 					<span className='header__optionLineOne'>Hello Guest</span>
+					<span className='header__optionLineTwo'>Sign In</span>
 				</div>
 				<div className='header__options'>
-					<span className='header__optionLineOne'>Return & Orders</span>
+					<span className='header__optionLineOne'>Returns </span>
+					<span className='header__optionLineTwo'>& Orders</span>
 				</div>
 				<div className='header__options'>
-					<span className='header__optionLineOne'>Your Prime</span>
+					<span className='header__optionLineOne'>Your</span>
+					<span className='header__optionLineTwo'>Prime</span>
+				</div>
+				<div className='header__optionBasket'>
+					<ShoppingBasketIcon />
+					<span className='header__optionLineTwo header__basketCount'>0</span>
 				</div>
 			</div>
 		</div>
