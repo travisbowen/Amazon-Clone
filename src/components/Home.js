1ca326479ec200f90011b8651dae7debdfa9420b
Travis Bowen <Travisbowen23@gmail.com>
2020-09-12 15:54:15 -0700

    Added dummy data for products on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,38 +20,38 @@ function Home() {
 						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
 					/>
 					<Product
-						title='The Lean Startup'
-						price={19.99}
+						title='Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl'
+						price={239.0}
 						rating={5}
-						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+						image='https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg'
 					/>
 				</div>
 				<div className='home__row'>
 					<Product
-						title='The Lean Startup'
-						price={19.99}
+						title='Fitbit with Heart Rate Tracker Generation 3'
+						price={199.99}
 						rating={1}
-						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+						image='https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg'
 					/>
 					<Product
-						title='The Lean Startup'
-						price={19.99}
+						title='Amazon Echo (3rd generation) Smart speaker with Alexa, Charcoal Fabric'
+						price={98.99}
 						rating={3}
-						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+						image='https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$'
 					/>
 					<Product
-						title='The Lean Startup'
-						price={19.99}
+						title='New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)'
+						price={598.98}
 						rating={4}
-						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+						image='https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg'
 					/>
 				</div>
 				<div className='home__row'>
 					<Product
-						title='The Lean Startup'
-						price={19.99}
+						title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+						price={1094.98}
 						rating={2}
-						image='https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg'
+						image='https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg'
 					/>
 				</div>
 			</div>
