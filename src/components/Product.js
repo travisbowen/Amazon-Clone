a2ec2a792f019866bc2d743925fa0204bd3968b2
Travis Bowen <Travisbowen23@gmail.com>
2020-09-14 12:28:54 -0700

    Finished subtotal pricing calculation setup

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "../utils/StateProvider";
 
 function Product({ id, title, price, rating, image }) {
+	const [{ basket }, dispatch] = useStateValue();
+
+	console.log("basket", basket);
+
+	const addToBasket = () => {
+		// Dispatch the item into the data layer
+		dispatch({
+			type: "ADD_TO_BASKET",
+			item: {
+				id: id,
+				title: title,
+				image: image,
+				price: price,
+				rating: rating,
+			},
+		});
+	};
+
 	return (
 		<div className='product'>
 			<div className='product__info'>
@@ -19,7 +38,9 @@ function Product({ id, title, price, rating, image }) {
 				</div>
 			</div>
 			<img className='product__image' src={image} alt='product' />
-			<button className='product__button'>Add to Basket</button>
+			<button className='product__button' onClick={addToBasket}>
+				Add to Basket
+			</button>
 		</div>
 	);
 }
--- src/utils/Reducer.js
+++ src/utils/Reducer.js
@@ -2,7 +2,11 @@ export const initialState = {
 	basket: [],
 };
 
+export const getBasketTotal = (basket) =>
+	basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
+	console.log(action);
 	switch (action.type) {
 		case "ADD_TO_BASKET":
 			return {
